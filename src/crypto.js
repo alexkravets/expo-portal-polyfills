005c250e63dbda726201c5169e50a38830bd8737
Oleksandr Kravets <a@kra.vc>
2021-02-06 09:39:14 +0200

    Fix createHash function

--- src/crypto.js
+++ src/crypto.js
@@ -2,13 +2,19 @@
 import sha256 from 'js-sha256'
 import { Buffer } from 'buffer'
 
-const createHash = () => {
+const createHash = (alg) => {
+  const isSupported = [ 'sha256' ].include(alg)
+
+  if (!isSupported) {
+    throw new Error(`"${alg}" is not supported`)
+  }
+
   return {
     update: (value) => {
-      const hash = sha256(value)
+      const hashHex = sha256(value)
 
       return {
-        digest: () => Buffer.from(hash)
+        digest: () => Buffer.from(hashHex, 'hex')
       }
     }
   }
